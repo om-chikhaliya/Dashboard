9fc9ab79a5af4a8438d6d637af513061eb1b8821
om-chikhaiya <om.chikhaliya@holbox.ai>
2025-02-18 14:27:00 +0530

    Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
@@ -15,27 +15,25 @@ import accessdenied from './assets/accessdenied2.png'
 const ProtectedRoute = ({ element, requiredRole }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const token = localStorage.getItem("accessToken");
-  const userRole = localStorage.getItem("role"); // Retrieve role from localStorage
+  const userRole = localStorage.getItem("role");
 
   if (!token) {
-    return <Navigate to="/" />; // Redirect to login if not authenticated
+    return <Navigate to="/" />;
   }
 
   if (requiredRole && userRole !== requiredRole) {
     return (<div className="app">
       <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
       <div className={`main-content lg:ml-[240px]`}>
-        
+
         <Header />
         <div className="flex justify-center items-center p-6">
-          {/* <div className=" rounded-lg p-8 w-full md:w-2/3 lg:w-2/4 min-h-[400px]"> */}
-            <img src={accessdenied} alt="" className="w-80 h-80 object-contain" />
-          {/* </div> */}
-          </div>
-        
+          <img src={accessdenied} alt="" className="w-80 h-80 object-contain" />
+        </div>
+
       </div>
     </div>)
-    // return <Navigate to="/dashboard" />; // Redirect to dashboard if role doesn't match
+
   }
 
   return element;
@@ -48,12 +46,12 @@ function App() {
       <div className="max-h-screen">
         <Routes>
           <Route path="/" element={<LoginSignup />} />
-          <Route path="/createuser" element={<ProtectedRoute element={<CreateUserForm />} requiredRole="admin"/>} />
+          <Route path="/createuser" element={<ProtectedRoute element={<CreateUserForm />} requiredRole="admin" />} />
           <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
           <Route path="/orders" element={<ProtectedRoute element={<OrderPage />} />} />
           <Route path="/pickorders" element={<ProtectedRoute element={<PickUpItemsPage />} />} />
           <Route path="/addkeys" element={<ProtectedRoute element={<ExpandingButtonForm />} />} />
-          <Route path="/setting" element={<ProtectedRoute element={<Setting />} requiredRole="admin"/>} />
+          <Route path="/setting" element={<ProtectedRoute element={<Setting />} requiredRole="admin" />} />
         </Routes>
       </div>
     </Router>
