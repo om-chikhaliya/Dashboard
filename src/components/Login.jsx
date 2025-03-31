920ca5af35ae3c1f24d4ed6005a401720dc58760
rupesh-holboxai <rupesh.yadav@holbox.ai>
2025-03-31 14:20:12 +0000

    changes after deployment

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -128,7 +128,7 @@ const LoginSignup = () => {
       try {
   
         // Login API Request
-        const loginResponse = await axios.post("http://localhost:4000/api/auth/login", {
+        const loginResponse = await axios.post("http://15.207.144.111:4000/api/auth/login", {
           email: loginData.email,
           password: loginData.password,
         });
@@ -169,7 +169,7 @@ const LoginSignup = () => {
     } else if (activeTab === 'signup' && validateSignup()) {
 
       // Send signup request using Axios
-      axios.post('http://localhost:4000/api/auth/register-admin', {
+      axios.post('http://15.207.144.111:4000/api/auth/register-admin', {
         email: signupData.email,
         password: signupData.password
       })
